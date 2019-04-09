3ead96c2aef797e31405885317aa17c9864cbd3c
Liyanan0410 <35992007+Liyanan0410@users.noreply.github.com>
2019-04-09 15:53:33 +0800

    first commit

---
+++ src/TestTransient.java
@@ -0,0 +1,58 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+* 关于JAVA里面序列化，反序列化以及transient的使用总结
+*/
+class Rectangle implements Serializable{
+    
+    private static final long serialVersionUID = 1710022455003682613L;
+    private Integer width;
+    private Integer height;
+    private transient Integer area;
+
+
+
+    public Rectangle (Integer width, Integer height){
+        this.width = width;
+        this.height = height;
+        this.area = width * height;
+    }
+
+    public void setArea(){
+        this.area = this.width * this.height;
+    }
+
+    @Override
+    public String toString(){
+        StringBuffer sb = new StringBuffer(40);
+        sb.append("width : ");
+        sb.append(this.width);
+        sb.append("\nheight : ");
+        sb.append(this.height);
+        sb.append("\narea : ");
+        sb.append(this.area);
+        return sb.toString();
+    }
+}
+public class TestTransient {
+	 public static void main(String args[]) throws Exception {
+	        Rectangle rectangle = new Rectangle(3,4);
+	        System.out.println("1.原始对象\n"+rectangle);
+	        ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream("rectangle"));
+	        // 往流写入对象
+	        o.writeObject(rectangle);
+	        o.close();
+
+	        // 从流读取对象
+	        ObjectInputStream in = new ObjectInputStream(new FileInputStream("rectangle"));
+	        Rectangle rectangle1 = (Rectangle)in.readObject();
+	        System.out.println("2.反序列化后的对象\n"+rectangle1);
+	        rectangle1.setArea();
+	        System.out.println("3.恢复成原始对象\n"+rectangle1);
+	        in.close();
+	    }
+}
---
+++ src/test.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class test {
+
+	public static void main(String[] args) {
+		HashMap<Integer, Integer>map = new HashMap<>();
+	}
+
+}
